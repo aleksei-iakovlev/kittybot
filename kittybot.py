7a1a2823ad6984b518e59b45557573f29999ec03
Aleksei Iakovlev <saint.alesha@ya.ru>
2026-04-18 12:47:16 +0300

    ready to publish

--- kittybot.py
+++ kittybot.py
@@ -1,46 +1,63 @@
-from telegram import Bot, ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+import logging
+import os
+
 import requests
 
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
 
-TELEGRAM_TOKEN = '8756892456:AAG_j24G8NH8gS3BtHh1jeaeVxe7LLbyk4E' 
-CHAT_ID = '1450780782'
-cat_url = 'https://api.thecatapi.com/v1/images/search'  
-#dog_url = 'https://api.thedogapi.com/v1/images/search'
+from dotenv import load_dotenv 
 
-bot = Bot(token=TELEGRAM_TOKEN)
-updater = Updater(token=TELEGRAM_TOKEN)
-button = ReplyKeyboardMarkup([['Показать кота']], resize_keyboard=True)
+load_dotenv()
 
+secret_token = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+URL = 'https://api.thecatapi.com/v1/images/search'
 
 def get_new_image():
-    response = requests.get(cat_url).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        # Печатать информацию в консоль теперь не нужно:
+        # всё необходимое будет в логах
+        # print(error)
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
 def new_cat(update, context):
     chat = update.effective_chat
-    name = update.message.chat.first_name
-    context.bot.send_message(
-        chat_id=chat.id,
-        text='Посмотри, какого котика я тебе нашёл'.format(name),
-        reply_markup=button
-    )
     context.bot.send_photo(chat.id, get_new_image())
 
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+
     context.bot.send_message(
         chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
         reply_markup=button
     )
+
     context.bot.send_photo(chat.id, get_new_image())
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('new_cat', new_cat))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, new_cat))
+def main():
+    updater = Updater(token=secret_token)
 
-updater.start_polling()
-updater.idle()
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
